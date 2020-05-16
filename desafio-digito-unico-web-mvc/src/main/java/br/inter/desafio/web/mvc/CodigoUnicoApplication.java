2af9730e92418718ad0224c7ae6813848532f83c
Rodolpho Erick <rodolphoerick@info.ufrn.br>
2020-05-16 00:55:35 -0300

    #Adicionando dependências para o swagger.

--- desafio-digito-unico-web-mvc/src/main/java/br/inter/desafio/web/mvc/CodigoUnicoApplication.java
+++ desafio-digito-unico-web-mvc/src/main/java/br/inter/desafio/web/mvc/CodigoUnicoApplication.java
@@ -12,10 +12,20 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
+import static springfox.documentation.builders.PathSelectors.any;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableSwagger2
 @ComponentScan("br.inter")
 @EnableTransactionManagement
 public class CodigoUnicoApplication extends SpringBootServletInitializer {
@@ -40,6 +50,17 @@ public class CodigoUnicoApplication extends SpringBootServletInitializer {
     @Bean
     public MessageSourceAccessor getMessageSourceAccessor(final MessageSource messageSource) {
         return new MessageSourceAccessor(messageSource, new Locale("pt_BR"));
+    }
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.inter.desafio.web.mvc")).paths(any()).build()
+                .apiInfo(new ApiInfo("Desafio Dígito Único - API", "Resolução para o desafio do dígito único.",
+                        "1.0", null,
+                        new Contact("LinkedIn", "https://www.linkedin.com/in/rodolphoerick/", "rodolphoess@gmail.com"),
+                        null, null, new ArrayList()));
     }
 
 }
